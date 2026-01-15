107e7080b82f1e1730fb072fc101f34fa664f7d9
Tai Doan <taidoan@live.co.uk>
2026-01-15 12:32:01 +0000

    feat(specials): Setup products for specials generator.

---
+++ config/products/descriptions.ts
@@ -0,0 +1,25 @@
+// DESCRIPTIONS
+const CHP_DESCRIPTION = "Served with chips.";
+const CHP_SLD_PLN_DESCRIPTION =
+  "Served with either chips, side salad or plain.";
+const SLD_DESCRIPTION = "Served with side salad.";
+
+export const PRODUCT_DESCRIPTIONS = {
+  BENEDICTS: "Served with rocket & hollandaise sauce.",
+  COD: "Served with either mushy peas, garden peas, baked beans or plain.",
+  GOURMET_BURGERS: "Served with chips & onion rings.",
+  CHIPS: CHP_DESCRIPTION,
+  BURGERS: CHP_DESCRIPTION,
+  LASAGNE: CHP_SLD_PLN_DESCRIPTION,
+  JACKET_POTATOES: SLD_DESCRIPTION,
+  SIMPLE_CURRY: "Served with either pilau rice or chips.",
+  CURRY: "Served with pilau rice, poppadums & naan bread.",
+  ALFREDO: "Garnished with rocket.",
+  NON_CARNE: "Served with Mexican rice & tortilla chips.",
+  RAMEN: "Garnished with coriander, chillies & red onion.",
+  PANINI: CHP_SLD_PLN_DESCRIPTION,
+  SAUSAGE_MASH: "Served with garden peas & gravy.",
+  CHICKEN_MELT: "Served with chips, garden peas & mushroom.",
+  SHAWARMA_CHIPS:
+    "Chips topped with chicken shawarma, hot sauce and garlic & herb sauce.",
+};
---
+++ config/products/discounts.ts
@@ -0,0 +1,6 @@
+export const PRODUCT_DISCOUNTS = {
+  MINIMUM: [1],
+  SMALL: [1, 2],
+  MEDIUM: [1, 2, 3],
+  MAX: [1, 2, 3, 4],
+};
