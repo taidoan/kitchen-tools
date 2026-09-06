ed599e3f61c5bc7e770bf60ba174410891c7e308
taidoan <taidoan@live.co.uk>
2026-09-06 16:32:08 +0100

    Revert "Added new products to specials"

--- config/products/products.ts
+++ config/products/products.ts
@@ -33,11 +33,6 @@ export const PRODUCT_GROUPS = [
     ],
   },
   {
-    products: ["Halloumi Burger"],
-    discounts: PRODUCT_DISCOUNTS.MAX,
-    description: ["Served with chips & sweet chilli sauce."],
-  },
-  {
     description: PRODUCT_DESCRIPTIONS.XMAS_BURGERS,
     discounts: PRODUCT_DISCOUNTS.MAX,
     products: [
@@ -123,11 +118,6 @@ export const PRODUCT_GROUPS = [
     products: ["Cod & Chips"],
   },
   {
-    description: PRODUCT_DESCRIPTIONS.COD,
-    discounts: PRODUCT_DISCOUNTS.MEDIUM,
-    products: ["Small Cod & Chips"],
-  },
-  {
     products: ["Alfredo Pasta"],
     description: PRODUCT_DESCRIPTIONS.ALFREDO,
     discounts: PRODUCT_DISCOUNTS.MAX,
@@ -168,18 +158,9 @@ export const PRODUCT_GROUPS = [
     discounts: PRODUCT_DISCOUNTS.MEDIUM,
   },
   {
-    products: ["Chicken & Broccoli"],
-    description: [
-      "Chicken breast served with mashed potato, peas, tenderstem broccoli & gravy.",
-    ],
-    discounts: PRODUCT_DISCOUNTS.MEDIUM,
-  },
-    {
     products: ["Steak & Ale Pudding"],
-    description: [
-      "Served with peas, gravy and either chips or mashed potato.",
-    ],
-    discounts: PRODUCT_DISCOUNTS.MEDIUM,
+    description: "Served with either chips or mash, peas & gravy.",
+    discounts: PRODUCT_DISCOUNTS.MAX,
   },
 ];
 
@@ -188,5 +169,5 @@ export const PRODUCTS = PRODUCT_GROUPS.flatMap((group) =>
     product: name,
     description: group.description,
     discounts: group.discounts,
-  })),
+  }))
 );
